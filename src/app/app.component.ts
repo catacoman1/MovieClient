7ff87a4ec49b07bc5beb6efd350db11722dffbd0
catacoman1 <catalin.coman@student.upt.ro>
2023-09-13 14:23:37 +0300

    Finished conectivity with backend api

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { GetApiService } from './get-api.service';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'movie-client';
+  constructor( private api:GetApiService)
+  {
+
+  }
+  ngOnInit()
+  {
+    this.api.apiCall().subscribe((data)=>{
+        console.warn("get api data", data);
+
+    })
+  }
+}
---
+++ src/app/get-api.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class GetApiService {
+  
+
+  constructor(
+    private http:HttpClient
+  ) { }
+  apiCall()
+  {
+    return this.http.get('http://localhost:8080/api/v1/movies');
+  }
+}
